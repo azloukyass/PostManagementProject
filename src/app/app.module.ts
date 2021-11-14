b9cde042e323e0185fa7cd11ba418799b30fa342
Azlouk Yassin <63109198+Azloukyassin@users.noreply.github.com>
2021-11-14 19:32:00 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MaterialModule} from './material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { DataService } from './data.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    DashbordComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/dashbord/dashbord.component.ts
@@ -0,0 +1,32 @@
+import { DataSource } from '@angular/cdk/collections';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-dashbord',
+  templateUrl: './dashbord.component.html',
+  styleUrls: ['./dashbord.component.css']
+})
+export class DashbordComponent implements OnInit {
+
+  constructor(private dataService:DataService) { }
+
+  displayedColumn=['date_posted', 'title', 'category', 'delete'];
+  dataSource= new PostDataSource(this.dataService);
+
+  ngOnInit(): void {
+  }
+
+}
+
+export class PostDataSource extends DataSource<any> {
+  constructor(private dataService:DataService){
+    super();
+  }
+
+  connect() : Observable<Post[]> {
+    return this.dataService.getData();
+  }
+  disconnect(){};
+}
